f46579b5d7636d4ea0fd3684f45309582060ff11
wiiliam <wiiliamchung360@gmail.com>
2015-07-24 10:25:19 -0700

    Made this script work

--- src/count.bat
+++ src/count.bat
@@ -3,12 +3,12 @@
 ::=== START ===
 :: Variables:
   :: `SET /P %Variables%="..." ` asks the user for an input.
-SET /P delay="Enter desired number of delay from 0.1 to 1 second(s): "
+SET /P delay="Enter desired number of delay 1+ second(s): "
 SET /P name="Enter the desired name of output file: "
   :: Set a initial number for the bat file
 SET num=1
 
-:: Variables without prompt:
+:: Static variables without prompt:
 SET targetdir=%homepath%/Desktop/ :: Dir. whrere we want the file to be in.
 SET number=%num%+1 :: Variable where we add 1 and append to on the file.
 
@@ -17,9 +17,9 @@ echo %num% > %name%    :: Create the file specified on line 7.
 
 :: The loop:
 :loop
-SET number=%num%+1
- :: ^^ Add one to the number prior to the one before it.
+echo %number% >> %name% :: Write number on last line of the file.
+TIMEOUT /T %delay% /NOBREAK > NUL          :: Delay for the amount specified on line 6.
 
-echo %command% >> %name% :: Write number on last line of the file.
-sleep %delay%            :: Delay for the amount specified on line 6.
-goto loop                :: End of loop
+SET number=%num%+1     :: Add one to the number prior to the one before it.
+
+goto loop              :: End of loop
