645de2afad25f7843bd2b5ae3fb6ddccd51c1618
Will <wiiliam@users.noreply.github.com>
2015-07-24 10:12:29 -0700

    Update count.bat

--- src/count.bat
+++ src/count.bat
@@ -5,17 +5,20 @@
   :: `SET /P %Variables%="..." ` asks the user for an input.
 SET /P delay="Enter desired number of delay from 0.1 to 1 second(s): "
 SET /P name="Enter the desired name of output file: "
+  :: Set a initial number for the bat file
+SET num=1
 
 :: Variables without prompt:
 SET targetdir=%homepath%/Desktop/ :: Dir. whrere we want the file to be in.
+SET number=%num%+1 :: Variable where we add 1 and append to on the file.
 
 cd %targetdir%       :: Go to the directory specified on line 10.
-echo "1" > %name%    :: Create the file specified on line 7.
+echo %num% > %name%    :: Create the file specified on line 7.
 
 :: The loop:
 :loop
-SET command={{ more %name% | tail -n 1} + 1 }}
- :: ^^ Read the last line of the file. Then add one to the number.
+SET number=%num%+1
+ :: ^^ Add one to the number prior to the one before it.
 
 echo %command% >> %name% :: Write number on last line of the file.
 sleep %delay%            :: Delay for the amount specified on line 6.
