581c45690ef2f0623cadb887d11e4865ab2f12b4
Will <wiiliam@users.noreply.github.com>
2015-07-28 08:49:07 -0700

    Update count.bat

--- src/count.bat
+++ src/count.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 :: A script that counts non-stop and writes the output into a file. Thus maxing out the (CPU and) storage space.
 
 ::=== START ===
@@ -6,20 +7,28 @@
 SET /P delay="Enter desired number of delay 1+ second(s): "
 SET /P name="Enter the desired name of output file: "
   :: Set a initial number for the bat file
-SET num=1
+SET /a num=1
 
 :: Static variables without prompt:
-SET targetdir=%homepath%/Desktop/ :: Dir. whrere we want the file to be in.
-SET number=%num%+1 :: Variable where we add 1 and append to on the file.
+SET targetdir=%homepath%/Desktop/ 
+  :: Dir. whrere we want the file to be in.
+SET /a number=2
+  :: Variable where we add 1 and append to on the file.
 
-cd %targetdir%       :: Go to the directory specified on line 10.
-echo %num% > %name%    :: Create the file specified on line 7.
+cd %targetdir%
+  :: Go to the directory specified on line 10.
+echo %num% > %name%
+  :: Create the file specified on line 7.
 
 :: The loop:
 :loop
-echo %number% >> %name% :: Write number on last line of the file.
-TIMEOUT /T %delay% /NOBREAK > NUL          :: Delay for the amount specified on line 6.
+echo %number% >> %name%
+  :: Write number on last line of the file.
+TIMEOUT /T %delay% /NOBREAK > NUL
+  :: Delay for the amount specified on line 6.
 
-SET number=%num%+1     :: Add one to the number prior to the one before it.
+SET /a number=%number%+1
+  :: Add one to the number prior to the one before it.
 
-goto loop              :: End of loop
+goto loop
+:: End of loop
